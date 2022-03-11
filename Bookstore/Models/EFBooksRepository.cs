e84aeb63f0358c6c0508432e4164a477a7f4abc7
Kevin <ktroll@byu.edu>
2022-03-11 11:54:42 -0700

    mission10final

---
+++ Bookstore/Models/EFBooksRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Bookstore.Models
+{
+    public class EFBooksRepository : IBooksRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBooksRepository(BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<Book> Books => context.Books;
+
+        public void SaveBook(Book b)
+        {
+            context.SaveChanges();
+        }
+
+        public void CreateBook(Book b)
+        {
+            context.Add(b);
+            context.SaveChanges();
+        }
+
+        public void DeleteBook(Book b)
+        {
+            context.Remove(b);
+            context.SaveChanges();
+        }
+
+    }
+}
